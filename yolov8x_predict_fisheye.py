f9bf0fdfc4f735166eaf028c4d5fa7b02d72d3d0
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-11 22:11:26 +0000

    use wandb's bounding box feature to log results

--- yolov8x_predict_fisheye.py
+++ yolov8x_predict_fisheye.py
@@ -11,24 +11,38 @@ print(f"total data for inference {len(sources)}")
 
 # %%
 run = wandb.init(project="yolov8-fisheye", name="yolov8x_predict")
-
-predictions_table = wandb.Table(columns=["filename", "prediction"])
+#predictions_table = wandb.Table(columns=["filename", "prediction"])
 
 model = YOLO('yolov8x.pt')
 results = model.predict(sources, save=False, imgsz=640, conf=0.5, stream=False)
-results_data = []
-for result in results:
-  image = cv2.cvtColor(result.plot(), cv2.COLOR_BGR2RGB)
-  plt.imshow(image)
-  plt.axis('off')
-  plt.title(result.path.rsplit('/',1)[-1])
-  buf = io.BytesIO()
-  plt.savefig(buf, format='png')
-  buf.seek(0)
-  results_data.append([result.path.rsplit('/',1)[-1],
-                       wandb.Image(Image.open(buf))])
-results_table = wandb.Table(data=results_data, columns=["filename", "prediction"])
-print(results_table)
 
-run.log({"prediction_table" : results_table})
+table = wandb.Table(columns=["ID", "Image"])
+for result in results:
+  img_id = result.path.rsplit('/',1)[-1]
+  print(id)
+  box_img = wandb.Image(
+    result.orig_img,
+    boxes={
+      "prediction": {
+        "box_data":
+      }
+    },
+  )
+  print(result.orig_img)
+
+  print(result.boxes)
+
+  table.add_data(img_id, box_img)
+  
+#  image = cv2.cvtColor(result.plot(), cv2.COLOR_BGR2RGB)
+#  plt.imshow(image)
+#  plt.axis('off')
+#  plt.title(result.path.rsplit('/',1)[-1])
+#  buf = io.BytesIO()
+#  plt.savefig(buf, format='png')
+#  buf.seek(0)
+#  results_data.append([result.path.rsplit('/',1)[-1],
+#                       wandb.Image(Image.open(buf))])
+
+run.log({"Table" : table})
 run.finish()
