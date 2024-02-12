6d26522860b8db843324ead4c881d26880f4ef6d
Yixiang Gao <yg5d6@umsystem.edu>
2024-02-12 19:55:58 +0000

    use wandb interface to log box results

--- yolov8x_predict_fisheye.py
+++ yolov8x_predict_fisheye.py
@@ -4,45 +4,50 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from ultralytics import YOLO
 
-# %%
-data_dir = 'dataset/Fisheye8K_all_including_train/test/images/'
-sources = [data_dir+img for img in os.listdir(data_dir)[:100]]
-print(f"total data for inference {len(sources)}")
+data_dir = '../dataset/Fisheye8K_all_including_train/test/images/'
+sources = [data_dir+img for img in os.listdir(data_dir)[:5]]
+print(f"Total data for inference {len(sources)}")
 
-# %%
 run = wandb.init(project="yolov8-fisheye", name="yolov8x_predict")
-#predictions_table = wandb.Table(columns=["filename", "prediction"])
 
 model = YOLO('yolov8x.pt')
 results = model.predict(sources, save=False, imgsz=640, conf=0.5, stream=False)
+names = model.names
+print(f"class names {model.names}")
 
 table = wandb.Table(columns=["ID", "Image"])
 for result in results:
   img_id = result.path.rsplit('/',1)[-1]
-  print(id)
+  print(img_id)
+  print(result.orig_img)
+
   box_img = wandb.Image(
     result.orig_img,
     boxes={
       "prediction": {
-        "box_data":
+        "box_data": [
+          {
+            "position": {
+              "minX": float(box.xyxyn.cpu().numpy()[0][0]),
+              "minY": float(box.xyxyn.cpu().numpy()[0][1]),
+              "maxX": float(box.xyxyn.cpu().numpy()[0][2]),
+              "maxY": float(box.xyxyn.cpu().numpy()[0][3]),
+            },
+            "class_id": int(box.cls.cpu().numpy()[0]),
+            "box_caption": names[int(box.cls.cpu().numpy()[0])],
+          }
+          for box in result.boxes
+        ],
+        "class_labels": names,
       }
     },
   )
-  print(result.orig_img)
-
-  print(result.boxes)
-
+  print(f"# of boxes {len(result.boxes)}")
+  for box in result.boxes:
+    print(box.xyxyn.cpu().numpy())
+    print(box.xyxyn.cpu().numpy()[0])
+    print(box.cls.cpu().numpy())
   table.add_data(img_id, box_img)
   
-#  image = cv2.cvtColor(result.plot(), cv2.COLOR_BGR2RGB)
-#  plt.imshow(image)
-#  plt.axis('off')
-#  plt.title(result.path.rsplit('/',1)[-1])
-#  buf = io.BytesIO()
-#  plt.savefig(buf, format='png')
-#  buf.seek(0)
-#  results_data.append([result.path.rsplit('/',1)[-1],
-#                       wandb.Image(Image.open(buf))])
-
 run.log({"Table" : table})
 run.finish()
