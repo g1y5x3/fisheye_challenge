e99272a84d5b2ff7b78b72d45630c665c73f6758
Yixiang Gao <yg5d6@umsystem.edu>
2024-02-13 00:53:43 +0000

    add eval script and remove the unclassified classes

--- yolov8x_eval_fisheye.py
+++ yolov8x_eval_fisheye.py
@@ -1,9 +1,10 @@
+# %%
 import io, os, cv2, wandb
 import matplotlib.pyplot as plt
 from PIL import Image
 from ultralytics import YOLO
 
-run = wandb.init(project="yolov8-fisheye", name="yolov8x_predict_test_128")
+run = wandb.init(project="yolov8-fisheye", name="yolov8x_predict_test_all")
 table = wandb.Table(columns=["ID", "Image"])
 
 data_dir = '../dataset/Fisheye8K_all_including_train/test/images/'
@@ -11,15 +12,13 @@ sources = [data_dir+img for img in os.listdir(data_dir)]
 print(f"Total data for inference {len(sources)}")
 
 model = YOLO('yolov8x.pt')
-#names = model.names
-names = {0: 'person', 2: 'car', 3: 'motorcycle', 5: 'bus', 7: 'truck'} 
-print(f"class names {names}")
+names = model.names
+print(f"class names {model.names}")
 
-#for i in range(len(sources)//128+1):
-for i in range(1):
+for i in range(len(sources)//128+1):
   start = i*128
   end = (i+1)*128 if i<=20 else -1 
-  results = model.predict(sources[start:end], classes=[0, 2, 3, 5, 7], save_txt=True, imgsz=640, conf=0.5, stream=True)
+  results = model.predict(sources[start:end], save=False, imgsz=640, conf=0.5, stream=True)
   for result in results:
     img_id = result.path.rsplit('/',1)[-1]
     box_img = wandb.Image(
