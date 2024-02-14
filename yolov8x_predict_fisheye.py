875fc07c9326a38af0377fa8f05caa0dc41739d4
Yixiang Gao <yg5d6@umsystem.edu>
2024-02-14 06:36:58 +0000

    minor clean up

--- yolov8x_predict_fisheye.py
+++ yolov8x_predict_fisheye.py
@@ -2,7 +2,7 @@ import io, os, cv2, wandb
 import matplotlib.pyplot as plt
 from PIL import Image
 from ultralytics import YOLO
-from ultralytics.utils.metrics import ConfusionMatrix
+from ultralytics.utils.metrics import ConfusionMatrix, DetMetrics, Metrics
 from utils import bounding_boxes 
 
 WANDB = os.getenv("WANDB", False)
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     run = wandb.init(project="fisheye-challenge", name="yolov8x_predict_test_128")
     table = wandb.Table(columns=["ID", "Image"])
   
-  data_dir = '../dataset/Fisheye8K_all_including_train/test/images/'
+  data_dir  = '../dataset/Fisheye8K_all_including_train/test/images/'
   label_dir = '../dataset/Fisheye8K_all_including_train/test/labels/'
   sources = [data_dir+img for img in os.listdir(data_dir)]
   print(f"Total data for inference {len(sources)}")
@@ -24,26 +24,24 @@ if __name__ == "__main__":
   model = YOLO('yolov8x.pt')
   conf_mat = ConfusionMatrix(5, conf=0.25, iou_thres=0.45, task="detect")
  
-  detections = np.zeros(128, 6)
-  gt_bboxes  = np.zeros(128, 6)
-  gt_cls     = np.zeros(128)
   #for i in range(len(sources)//128+1):
   for i in range(1):
     # starting and ending indices for each batch
     start = i*128
     end = (i+1)*128 if i<=20 else -1 
+
     results = model.predict(sources[start:end], classes=[0, 2, 3, 5, 7], imgsz=640, conf=0.0, iou=0.7, stream=True)
+
     for result in results:
       img_id = result.path.rsplit('/',1)[-1]
       print(img_id)
 
-      # TODO: do I need to save this or just use boxes to retrieve value for benchmarks calculation?
-      result.save_txt("results/" + img_id.replace(".png", ".txt"))
-
       # Load the groundtruth for corresponding image - [x, y, width, height]
-      boxes_gt = []
       with open(label_dir + img_id.replace(".png", ".txt"), "r") as file:
         boxes_gt = file.readlines()
+
+      # NOTE: an alternative way is to save the predictions as json file and call eval_json() from val.py
+
       
       if WANDB:
         box_img = bounding_boxes(result.orig_img, result.boxes, boxes_gt, class_coco, classid_fisheye)
