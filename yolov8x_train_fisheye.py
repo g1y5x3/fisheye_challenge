cd7026ace046c747172e3b789dd8fe3cf7ccde62
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-15 17:24:33 +0000

    enable multigpu

--- yolov8x_train_fisheye.py
+++ yolov8x_train_fisheye.py
@@ -1,10 +1,18 @@
-import wandb
+import wandb, argparse
 from ultralytics import YOLO
 
-wandb.init(project="fisheye-challenge", name="yolov8x_train")
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="yolov8x fisheye experiment")
+  parser.add_argument('-devices', type=int, default=1, help="batch size")
+  args = parser.parse_args()
 
-model = YOLO('yolov8x.pt') # model was pretrained on COCO dataset
-
-results = model.train(data="fisheye.yaml", epochs=1, batch=16, imgsz=640)
-
-wandb.finish()
+  arg = argparse
+  wandb.init(project="fisheye-challenge", name="yolov8x_train")
+  
+  devices = args.devices
+  model = YOLO('yolov8x.pt') # model was pretrained on COCO dataset
+  
+  # TODO: too much abstracted details
+  results = model.train(data="fisheye.yaml", device=[i for i in range(devices)], epochs=1, batch=16, imgsz=640)
+  
+  wandb.finish()
