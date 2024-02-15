eb2055a88107d23f8c4fa49437974df616c3762f
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-15 15:41:05 +0000

    provide seom more info in the training

--- yolov8x_train_fisheye.py
+++ yolov8x_train_fisheye.py
@@ -5,6 +5,6 @@ wandb.init(project="fisheye-challenge", name="yolov8x_train")
 
 model = YOLO('yolov8x.pt') # model was pretrained on COCO dataset
 
-results = model.train(data="fisheye.yaml", epochs=1, imgsz=640)
+results = model.train(data="fisheye.yaml", epochs=1, batch=16, imgsz=640)
 
 wandb.finish()
