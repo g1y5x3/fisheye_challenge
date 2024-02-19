7be311a76ec9482d7a9cce6b99a84941ba02abdd
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-19 22:37:31 +0000

    locate the parts that's useful, need refactor

--- internimage_eval_fisheye.py
+++ internimage_eval_fisheye.py
@@ -24,6 +24,70 @@ from mmdet.models import build_detector
 import mmdet_custom  # noqa: F401,F403
 import mmcv_custom  # noqa: F401,F403
 
+"""
+def single_gpu_test(model,
+                    data_loader,
+                    show=False,
+                    out_dir=None,
+                    show_score_thr=0.3):
+    model.eval()
+    results = []
+    dataset = data_loader.dataset
+    PALETTE = getattr(dataset, 'PALETTE', None)
+    prog_bar = mmcv.ProgressBar(len(dataset))
+    for i, data in enumerate(data_loader):
+        with torch.no_grad():
+            result = model(return_loss=False, rescale=True, **data)
+
+        batch_size = len(result)
+        if show or out_dir:
+            if batch_size == 1 and isinstance(data['img'][0], torch.Tensor):
+                img_tensor = data['img'][0]
+            else:
+                img_tensor = data['img'][0].data[0]
+            img_metas = data['img_metas'][0].data[0]
+            imgs = tensor2imgs(img_tensor, **img_metas[0]['img_norm_cfg'])
+            assert len(imgs) == len(img_metas)
+
+            for i, (img, img_meta) in enumerate(zip(imgs, img_metas)):
+                h, w, _ = img_meta['img_shape']
+                img_show = img[:h, :w, :]
+
+                ori_h, ori_w = img_meta['ori_shape'][:-1]
+                img_show = mmcv.imresize(img_show, (ori_w, ori_h))
+
+                if out_dir:
+                    out_file = osp.join(out_dir, img_meta['ori_filename'])
+                else:
+                    out_file = None
+
+                model.module.show_result(
+                    img_show,
+                    result[i],
+                    bbox_color=PALETTE,
+                    text_color=PALETTE,
+                    mask_color=PALETTE,
+                    show=show,
+                    out_file=out_file,
+                    score_thr=show_score_thr)
+
+        # encode mask results
+        if isinstance(result[0], tuple):
+            result = [(bbox_results, encode_mask_results(mask_results))
+                      for bbox_results, mask_results in result]
+        # This logic is only used in panoptic segmentation test.
+        elif isinstance(result[0], dict) and 'ins_results' in result[0]:
+            for j in range(len(result)):
+                bbox_results, mask_results = result[j]['ins_results']
+                result[j]['ins_results'] = (bbox_results,
+                                            encode_mask_results(mask_results))
+
+        results.extend(result)
+
+        for _ in range(batch_size):
+            prog_bar.update()
+    return results
+"""
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -147,10 +211,12 @@ def main():
                 cfg.model.neck.rfp_backbone.pretrained = None
 
     # in case the test dataset is concatenated
-    samples_per_gpu = 1
+    samples_per_gpu = 16
     if isinstance(cfg.data.test, dict):
         cfg.data.test.test_mode = True
         samples_per_gpu = cfg.data.test.pop('samples_per_gpu', 1)
+        print(f"samples per gpu {samples_per_gpu}")
+        samples_per_gpu = 16
         if samples_per_gpu > 1:
             # Replace 'ImageToTensor' to 'DefaultFormatBundle'
             cfg.data.test.pipeline = replace_ImageToTensor(
@@ -160,6 +226,7 @@ def main():
             ds_cfg.test_mode = True
         samples_per_gpu = max(
             [ds_cfg.pop('samples_per_gpu', 1) for ds_cfg in cfg.data.test])
+        print(f"samples per gpu {samples_per_gpu}")
         if samples_per_gpu > 1:
             for ds_cfg in cfg.data.test:
                 ds_cfg.pipeline = replace_ImageToTensor(ds_cfg.pipeline)
@@ -192,6 +259,7 @@ def main():
         json_file = osp.join(args.work_dir, f'eval_{timestamp}.json')
 
     # build the dataloader
+    print(cfg.data.test)
     dataset = build_dataset(cfg.data.test)
     data_loader = build_dataloader(dataset,
                                    samples_per_gpu=samples_per_gpu,
@@ -233,23 +301,23 @@ def main():
         if args.out:
             print(f'\nwriting results to {args.out}')
             mmcv.dump(outputs, args.out)
-        kwargs = {} if args.eval_options is None else args.eval_options
-        if args.format_only:
-            dataset.format_results(outputs, **kwargs)
-        if args.eval:
-            eval_kwargs = cfg.get('evaluation', {}).copy()
-            # hard-code way to remove EvalHook args
-            for key in [
-                    'interval', 'tmpdir', 'start', 'gpu_collect', 'save_best',
-                    'rule', 'dynamic_intervals'
-            ]:
-                eval_kwargs.pop(key, None)
-            eval_kwargs.update(dict(metric=args.eval, **kwargs))
-            metric = dataset.evaluate(outputs, **eval_kwargs)
-            print(metric)
-            metric_dict = dict(config=args.config, metric=metric)
-            if args.work_dir is not None and rank == 0:
-                mmcv.dump(metric_dict, json_file)
+        #kwargs = {} if args.eval_options is None else args.eval_options
+        #if args.format_only:
+        #    dataset.format_results(outputs, **kwargs)
+        #if args.eval:
+        #    eval_kwargs = cfg.get('evaluation', {}).copy()
+        #    # hard-code way to remove EvalHook args
+        #    for key in [
+        #            'interval', 'tmpdir', 'start', 'gpu_collect', 'save_best',
+        #            'rule', 'dynamic_intervals'
+        #    ]:
+        #        eval_kwargs.pop(key, None)
+        #    eval_kwargs.update(dict(metric=args.eval, **kwargs))
+        #    metric = dataset.evaluate(outputs, **eval_kwargs)
+        #    print(metric)
+        #    metric_dict = dict(config=args.config, metric=metric)
+        #    if args.work_dir is not None and rank == 0:
+        #        mmcv.dump(metric_dict, json_file)
 
 
 if __name__ == '__main__':
