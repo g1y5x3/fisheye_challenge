ca05ac5db5cadab949b8d4512450afec5abf0e0a
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-21 23:38:22 +0000

    inspect internimage json results done

--- utils.py
+++ utils.py
@@ -1,5 +1,47 @@
 import wandb
+import numpy as np
 from PIL import Image
+from ultralytics.utils import ops
+from ultralytics.utils.plotting import Annotator, Colors
+
+def plot_images(images, cls, bboxes, confs, names):
+  bs = 1
+  colors = Colors()
+  ns = np.ceil(bs**0.5)
+  h, w, _ = images.shape 
+  mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
+  i = 0
+  x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
+  mosaic[y : y + h, x : x + w, :] = images
+  
+  # Resize
+  max_size = 1920
+  scale = max_size / ns / max(h, w)
+  
+  # Annotate
+  fs = int((h + w) * ns * 0.005)  # font size
+  annotator = Annotator(images, line_width=round(fs / 10), font_size=fs, pil=True, example=None)
+  for i in range(bs):
+    x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
+    annotator.rectangle([x, y, x + w, y + h], None, (255, 255, 255), width=2)  # borders
+    if len(cls) > 0:
+      classes = cls.astype("int")
+      labels = confs is None
+      
+      if len(bboxes):
+        boxes = bboxes
+        conf = confs if confs is not None else None  # check for confidence presence (label vs pred)
+        boxes = ops.ltwh2xyxy(boxes)
+        boxes[..., 0::2] += x
+        boxes[..., 1::2] += y
+        for j, box in enumerate(boxes.astype(np.int64).tolist()):
+          c = classes[j]
+          color = colors(c)
+          c = names[c] if names else c
+          if labels or conf[j] > 0.25:  # 0.25 conf thresh
+            label = f"{c}" if labels else f"{c} {conf[j]:.1f}"
+            annotator.box_label(box, label, color=color, rotated=None)
+  annotator.show()
 
 def bounding_boxes(img, boxes_predict, boxes_gt, class_name, classid_coco, classid_fisheye):
   return wandb.Image(
