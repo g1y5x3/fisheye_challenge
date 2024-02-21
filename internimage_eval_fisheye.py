58e11203a87ceb1cb5fed1ec7fd78b5a21f558e9
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-21 15:17:49 +0000

    save internimage results in json

--- internimage_eval_fisheye.py
+++ internimage_eval_fisheye.py
@@ -4,7 +4,7 @@
 # Licensed under The MIT License [see LICENSE for details]
 # --------------------------------------------------------
 
-import argparse, warnings, mmcv, torch
+import requests, argparse, warnings, mmcv, torch
 
 from mmcv import Config, DictAction
 from mmcv.cnn import fuse_conv_bn
@@ -16,8 +16,8 @@ from mmdet.models import build_detector
 # TODO: make more specific imports
 import mmdet_custom  # noqa: F401,F403
 import mmcv_custom  # noqa: F401,F403
+from pathlib import Path
 
-# just to reduce the number of batches for quick testing, the last three arguments are not used
 def single_gpu_4batch_test(model, data_loader, show=False, out_dir=None, show_score_thr=0.3):
   model.eval()
   results = []
@@ -27,61 +27,44 @@ def single_gpu_4batch_test(model, data_loader, show=False, out_dir=None, show_sc
   data_iterator = iter(data_loader)
   for i in range(4):
     data = next(data_iterator)
-    with torch.no_grad():
+    with torch.no_grad(): 
       result = model(return_loss=False, rescale=True, **data)
-
-    batch_size = len(result)
-
-    # The mask head is removed so there's no need to encode mask results
     results.extend(result)
 
-    for _ in range(batch_size):
+    # The mask head is removed so there's no need to encode mask results
+    for _ in range(len(result)): 
       prog_bar.update()
 
   return results
 
-"""Convert detection results to COCO json style."""
-#def det2json(results):
-#  json_results = []
-#  for idx in range(128):
-#    img_id = self.img_ids[idx]
-#    result = results[idx]
-#    for label in range(len(result)):
-#      bboxes = result[label]
-#      for i in range(bboxes.shape[0]):
-#        data = dict()
-#        data['image_id'] = img_id
-#        data['bbox'] = self.xyxy2xywh(bboxes[i])
-#        data['score'] = float(bboxes[i][4])
-#        data['category_id'] = self.cat_ids[label]
-#        json_results.append(data)
-#  return json_results
+def det2json(dataset, results):
+  json_results = []
+  for idx in range(128):
+    img_id = dataset.img_ids[idx]
+    #filename = dataset.coco.load_imgs(img_id)[0]['file_name']
+    result = results[idx]
+    for label in range(len(result)):
+      if label in [0,2,3,5,7]:
+        bboxes = result[label]
+        for i in range(bboxes.shape[0]):
+          data = dict()
+          data['image_id'] = img_id
+          # TODO: convert the bbox into normalized coordinates
+          data['bbox'] = dataset.xyxy2xywh(bboxes[i])
+          data['score'] = float(bboxes[i][4])
+          data['category_id'] = label
+          json_results.append(data)
+  return json_results
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description='MMDet test (and eval) a model')
+    parser = argparse.ArgumentParser(description='MMDet test (and eval) a model')
     parser.add_argument('config', help='test config file path')
-    parser.add_argument('checkpoint', help='checkpoint file')
     parser.add_argument(
         '--eval',
         type=str,
         nargs='+',
         help='evaluation metrics, which depends on the dataset, e.g., "bbox",'
         ' "segm", "proposal" for COCO, and "mAP", "recall" for PASCAL VOC')
-    parser.add_argument(
-        '--tmpdir',
-        help='tmp directory used for collecting results from multiple '
-        'workers, available when gpu-collect is not specified')
-    parser.add_argument(
-        '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
-        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
-        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
     parser.add_argument(
         '--options',
         nargs='+',
@@ -110,21 +93,13 @@ def parse_args():
         args.eval_options = args.options
     return args
 
-
-
-def main():
+if __name__ == '__main__':
+  # TODO: need a better way to handle different configs, this api is awful
   args = parse_args()
-
   cfg = Config.fromfile(args.config)
-  if args.cfg_options is not None:
-    cfg.merge_from_dict(args.cfg_options)
-  # set cudnn_benchmark
-  if cfg.get('cudnn_benchmark', False):
-    torch.backends.cudnn.benchmark = True
-
+  torch.backends.cudnn.benchmark = True
   cfg.model.pretrained = None
   cfg.data.test.test_mode = True
-  cfg.gpu_ids = range(1)
 
   # build the dataloader
   dataset = build_dataset(cfg.data.test)
@@ -134,41 +109,22 @@ def main():
                                  dist=False,
                                  shuffle=False)
 
-  # build the model and load checkpoint
+  # build the model and load weights
+  checkpoint = "checkpoints/cascade_internimage_xl_fpn_3x_coco.pth"
+  if not Path(checkpoint).exists():
+    print("Model checkpoint doesn't exist. Downloading...")
+    response = requests.get("https://huggingface.co/OpenGVLab/InternImage/resolve/main/cascade_internimage_xl_fpn_3x_coco.pth")
+    Path(checkpoint).write_bytes(response.content)
+
+  # initialize the pre-trained model
   cfg.model.train_cfg = None
   model = build_detector(cfg.model) # cfg.model contains the threshold for nms as well as score threshold
-  checkpoint = load_checkpoint(model, args.checkpoint, map_location='cpu')
+  checkpoint = load_checkpoint(model, checkpoint, map_location='cpu')
   model = fuse_conv_bn(model)
   model.CLASSES = dataset.CLASSES
-  model = MMDataParallel(model, device_ids=cfg.gpu_ids)
+  model = MMDataParallel(model, device_ids=[0])
 
-  # predict bounding boxes
+  # predict bounding boxes and save results to json
   outputs = single_gpu_4batch_test(model, data_loader, show=False, out_dir=None, show_score_thr=0.3)
-  mmcv.dump(outputs, "results/internimage_eval.pkl")
-
-  print(dataset.img_ids)
-  print(dataset.cat_ids)
-  #dataset.format_results(outputs, jsonfile_prefix="results")
-  dataset.results2json(outputs, "results/internimage")
-
-  #TODO: Retrieve the image id with the test image sequences and upload to wandb using val.py
-  #kwargs = {} if args.eval_options is None else args.eval_options
-  #if args.format_only:
-  #    dataset.format_results(outputs, **kwargs)
-  #if args.eval:
-  #    eval_kwargs = cfg.get('evaluation', {}).copy()
-  #    # hard-code way to remove EvalHook args
-  #    for key in [
-  #            'interval', 'tmpdir', 'start', 'gpu_collect', 'save_best',
-  #            'rule', 'dynamic_intervals'
-  #    ]:
-  #        eval_kwargs.pop(key, None)
-  #    eval_kwargs.update(dict(metric=args.eval, **kwargs))
-  #    metric = dataset.evaluate(outputs, **eval_kwargs)
-  #    print(metric)
-  #    metric_dict = dict(config=args.config, metric=metric)
-  #    if args.work_dir is not None and rank == 0:
-  #        mmcv.dump(metric_dict, json_file)
-
-if __name__ == '__main__':
-    main()
+  results_json = det2json(dataset, outputs)
+  mmcv.dump(results_json, "results/internimage_eval.bbox.json")
