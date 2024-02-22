d6fc19ebd8c0fbbdb9014f087b46541ce1bf007c
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-22 16:41:41 +0000

    put Detector back to utils

--- utils.py
+++ utils.py
@@ -1,9 +1,61 @@
+import torch
 import wandb
 import numpy as np
 from PIL import Image
 from ultralytics.utils import ops
 from ultralytics.utils.plotting import Annotator, Colors
+from ultralytics.utils.metrics import ConfusionMatrix, DetMetrics
+from ultralytics.models.yolo.detect.val import DetectionValidator
 
+# modifiedy from ultralytics to make it more framework agnostic, the original function was too tie
+# to batch processing workflow
+# https://github.com/ultralytics/ultralytics/blob/main/ultralytics/models/yolo/detect/val.py
+# 
+# preds [[x, y, x, y, conf, cls], ...]: list of torch.tensors [N, 6]
+# gts   [[x, y, x, y, cls], ...]      : list of torch.tensors [M, 5]
+#
+class FisheyeDetectionValidator(DetectionValidator):
+  def __init__(self, dataloader=None, save_dir=None, pbar=None, args=None, _callbacks=None):
+    super().__init__(dataloader, save_dir, pbar, args, _callbacks)
+    self.training = False
+
+  def init_metrics(self, names):
+    self.names = names
+    self.nc = len(names)
+    self.metrics.names = self.names
+    # default: conf=0.25, iou_thres=0.45
+    self.confusion_matrix = ConfusionMatrix(nc=self.nc, task="detect")
+    self.stats = dict(tp=[], conf=[], pred_cls=[], target_cls=[])
+    self.seen = 0
+
+  # TODO: refactor this
+  def update_metrics(self, preds, gts):
+    for pred, gt in zip(preds, gts):
+      self.seen += 1
+      npr = len(pred)
+      stat = dict(
+        conf=torch.zeros(0),
+        pred_cls=torch.zeros(0),
+        tp=torch.zeros(npr, self.niou, dtype=torch.bool),
+      )
+      bbox, cls = gt[:, :4], gt[:,4]
+      nl = len(cls)
+      stat["target_cls"] = cls
+      if npr == 0:
+        if nl:
+          for k in self.stats.keys():
+            self.stats[k].append(stat[k])
+          self.confusion_matrix.process_batch(detections=None, gt_bboxes=bbox, gt_cls=cls)
+      else:
+        stat["conf"], stat["pred_cls"] = pred[:, 4], pred[:, 5]
+        if nl:
+          stat["tp"] = self._process_batch(pred, bbox, cls)
+          self.confusion_matrix.process_batch(detections=pred, gt_bboxes=bbox, gt_cls=cls)
+        for k in self.stats.keys():
+          self.stats[k].append(stat[k])
+
+# modified from 
+# 
 def plot_images(images, cls, bboxes, confs, names, title=None, conf_thres=0.25, plot=False):
   # TODO: plot multiple images
   bs = 1
