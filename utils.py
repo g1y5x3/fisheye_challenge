1227ff8972459a46a4f21eaaffa5f3e9a19c2fb5
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-23 20:33:54 +0000

    update training script

--- utils.py
+++ utils.py
@@ -140,3 +140,12 @@ def bounding_boxes(img, boxes_predict, boxes_gt, class_name):
              }
            },
         ) 
+
+def get_image_Id(img_name):
+  #img_name = img_name.split('.png')[0]
+  sceneList = ['M', 'A', 'E', 'N']
+  cameraIndx = int(img_name.split('_')[0].split('camera')[1])
+  sceneIndx = sceneList.index(img_name.split('_')[1])
+  frameIndx = int(img_name.split('_')[2])
+  imageId = int(str(cameraIndx)+str(sceneIndx)+str(frameIndx))
+  return imageId
--- yolov8x_train_fisheye.py
+++ yolov8x_train_fisheye.py
@@ -1,7 +1,10 @@
 """
+For distributed training, run the following command
 python -m torch.distributed.run --nproc_per_node 2 yolov8x_train_fisheye.py -devices 2 -epoch 1 -bs 32
 """
 import wandb, argparse
+from evaluation.pycocotools.coco import COCO
+from evaluation.pycocotools.cocoeval import COCOeval
 from ultralytics.models.yolo.detect.train import DetectionTrainer
 
 if __name__ == "__main__":
@@ -12,6 +15,10 @@ if __name__ == "__main__":
   args = parser.parse_args()
   
   device = 0 if args.devices == 1 else [i for i in range(args.devices)]
-  train_args = dict(model="checkpoints/yolov8x.pt", data="fisheye.yaml", device=device, epochs=args.epoch, batch=args.bs, imgsz=640)
+
+  train_args = dict(model="checkpoints/yolov8x.pt", data="fisheye.yaml",
+                    device=device, epochs=args.epoch, batch=args.bs, imgsz=640,
+                    project="fisheye-challenge", name="yolov8x",
+                    val=False, save_json=True)
   trainer = DetectionTrainer(overrides=train_args)
   trainer.train()
