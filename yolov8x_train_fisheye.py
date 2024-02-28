600c8232c335407380d3ae5b92ec8e655cd0cc4e
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-28 23:52:03 +0000

    perform normal training

--- yolov8x_train_fisheye.py
+++ yolov8x_train_fisheye.py
@@ -231,11 +231,11 @@ if __name__ == "__main__":
   
   device = 0 if args.devices == 1 else [i for i in range(args.devices)]
 
-  train_args = dict(model="yolov8x.yaml", data="fisheye.yaml",
+  train_args = dict(project=args.project, name=args.name,
+                    model="yolov8x.yaml", data="fisheye.yaml",
                     device=device, epochs=args.epoch, batch=args.bs, fraction=args.frac, imgsz=1280,
-                    project=args.project, name=args.name,
                     val=True, save_json=True, exist_ok=True,
-                    box=7.5, cls=0.125, dfl=3.0,
+                    box=7.5, cls=0.125, dfl=5.0,
                     close_mosaic=0, # completely disable mosaic
                     degrees=0.1, translate=0.1, scale=0.0, shear=0.0, 
                     perspective=0.0, flipud=0.0, fliplr=0.5, 
