4fa1831425722aa354c8ea916f6ab204dbd1c3bb
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-28 21:26:41 +0000

    try different gain for losses

--- yolov8x_train_fisheye.py
+++ yolov8x_train_fisheye.py
@@ -164,6 +164,7 @@ if __name__ == "__main__":
                     device=device, epochs=args.epoch, batch=args.bs, fraction=args.frac, imgsz=1280,
                     project=args.project, name=args.name,
                     val=True, save_json=True, exist_ok=True,
+                    box=7.5, cls=0.125, dfl=3.0,
                     close_mosaic=0, # completely disable mosaic
                     degrees=0.1, translate=0.1, scale=0.0, shear=0.0, 
                     perspective=0.0, flipud=0.0, fliplr=0.5, 
