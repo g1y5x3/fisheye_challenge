926069cede9594d6dd0e37d8d1a2fba710c619e1
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-02-29 18:40:18 +0000

    fix typo

--- yolov8x_train_fisheye.py
+++ yolov8x_train_fisheye.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
                     model="yolov8x_dcn.yaml", data="fisheye.yaml",
                     device=device, epochs=args.epoch, batch=args.bs, fraction=args.frac, imgsz=1280,
                     exist_ok=True,
-                    val=True, save_json=True, conf=0.5, iou=0.5
+                    val=True, save_json=True, conf=0.5, iou=0.5,
                     optimizer="Adam", seed=0,
                     box=7.5, cls=0.125, dfl=3.0,
                     close_mosaic=0,
