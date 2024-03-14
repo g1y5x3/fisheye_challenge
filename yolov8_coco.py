dbe922782dab60af90919f5021ef4a1213949c1a
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-03-14 23:01:04 +0000

    fix bug in simple script

--- yolov8_coco.py
+++ yolov8_coco.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
+import argparse
 
-model = YOLO('yolov8x-p2.yaml')
 
 parser = argparse.ArgumentParser(description="yolov8x")
 parser.add_argument('-model', type=str, default="yolov8x-p2.yaml", help="model file name")
 args = parser.parse_args()
  
-results = model.train(data=args.model, epochs=250, imgsz=1024)
+model = YOLO(args.model)
+results = model.train(data='coco.yaml', epochs=250, imgsz=1024)
