a41c3f594cc6479c47fef8350073b167341037a4
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-03-24 23:47:00 +0000

    new pipeline tested

--- shuffle_data.py
+++ shuffle_data.py
@@ -4,22 +4,22 @@ from pathlib import Path
 train_dir = Path("/workspace/FishEye8k/dataset/Fisheye8K_all_including_train/train/images")
 test_dir = Path("/workspace/FishEye8k/dataset/Fisheye8K_all_including_train/test/images")
 
-#for file in test_dir.glob("*"):
-#  src_file = test_dir / file.name
-#  dest_file = train_dir / file.name
-#
-#  shutil.move(str(src_file), str(dest_file))
-#  print(f'Moved {file.name} to {train_dir}')
+for file in test_dir.glob("*"):
+  src_file = test_dir / file.name
+  dest_file = train_dir / file.name
+
+  shutil.move(str(src_file), str(dest_file))
+  print(f'Moved {file.name} to {train_dir}')
 
 train_dir = Path("/workspace/FishEye8k/dataset/Fisheye8K_all_including_train/train/labels")
 test_dir = Path("/workspace/FishEye8k/dataset/Fisheye8K_all_including_train/test/labels")
 
-#for file in test_dir.glob("*"):
-#  src_file = test_dir / file.name
-#  dest_file = train_dir / file.name
-#
-#  shutil.move(str(src_file), str(dest_file))
-#  print(f'Moved {file.name} to {train_dir}')
+for file in test_dir.glob("*"):
+  src_file = test_dir / file.name
+  dest_file = train_dir / file.name
+
+  shutil.move(str(src_file), str(dest_file))
+  print(f'Moved {file.name} to {train_dir}')
 
 random.seed(42)
 count = 0
