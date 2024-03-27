94513d9d339c6f1f6a3340cc72b285c931537cba
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-03-27 00:27:53 -0500

    disable mosaic

--- rtdetr-x_train_fisheye.py
+++ rtdetr-x_train_fisheye.py
@@ -46,10 +46,10 @@ if __name__ == "__main__":
                     lr0=2e-5, warmup_bias_lr=2e-5/3, weight_decay=args.wd,
                     optimizer="AdamW", seed=0,
                     box=7.5, cls=0.5, dfl=1.5,
-                    close_mosaic=10,
+                    close_mosaic=0,
                     degrees=0.0, translate=0.1, scale=0.0, shear=0.0,
                     perspective=0.0, flipud=0.0, fliplr=0.5, 
-                    mosaic=1.0, mixup=0.0,
+                    mosaic=0.0, mixup=0.0,
                     deterministic=True, verbose=True,
                     pretrained=True)
 
