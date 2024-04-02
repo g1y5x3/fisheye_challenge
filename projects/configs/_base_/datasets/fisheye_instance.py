806505d586ea16264e8979d7962d44a5ab0d15e0
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-04-02 22:56:50 +0000

    add configs and scripts for co-detr

---
+++ projects/configs/_base_/datasets/fisheye_instance.py
@@ -0,0 +1,53 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+classes = ("Bus", "Bike", "Car", "Pedestrian", "Truck")
+data_root = '/workspace/FishEye8k/dataset/Fisheye8K_all_including_train/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train/train.json',
+        img_prefix=data_root + 'train/images/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/test.json',
+        img_prefix=data_root + 'test/images/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/test.json',
+        img_prefix=data_root + 'test/images/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric=['bbox'], classwise=True)
